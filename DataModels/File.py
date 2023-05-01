4a451935c612bafcb760037d36d3ad72b9699b7b
s442333 <konpie1@st.amu.edu.pl>
2023-05-01 02:03:06 +0200

    add info about image in File model

--- DataModels/File.py
+++ DataModels/File.py
@@ -1,4 +1,7 @@
-from os.path import basename
+from os.path import basename, getctime
+import datetime
+
+from PIL import Image
 
 
 class File:
@@ -6,3 +9,22 @@ class File:
         self.path = path
 
         self.name = basename(path)
+
+        if self._isImage():
+            with Image.open(self.path) as img:
+                self.width, self.height = img.size
+                time = getctime(self.path)
+                time = datetime.datetime.fromtimestamp(
+                    time).strftime('%Y-%m-%d %H:%M:%S')
+
+                self.time = time
+
+            print(self.width, self.height, self.time)
+
+    def _isImage(self) -> bool:
+        try:
+            with Image.open(self.path) as img:
+                img.verify()
+            return True
+        except:
+            return False
