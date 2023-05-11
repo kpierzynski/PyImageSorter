3566cd003f19c47203319936ac14a3c87d89d21d
s442333 <konpie1@st.amu.edu.pl>
2023-05-11 02:57:11 +0200

    Handle exit app on ctrl+c

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from PySide6.QtWidgets import QApplication
 from MainWindow import MainWindow
 
 import sys
+import signal
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 app.exec()
